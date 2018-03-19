4cd386611965c06839816bf7148eaeff261e7f8d
Nalin Suri <nalinsuri@gmail.com>
2018-03-19 09:44:40 -0400

    Nodemon reload only on server change. Import index css

--- client/components/App.js
+++ client/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">React MERN Boilerplate</h1>
+          <h1 className="App-title">MERN Starter</h1>
         </header>
       </div>
     );
